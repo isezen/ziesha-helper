f98e239643fcbc62a449defd7bc14badb3e074be
Ismail SEZEN <613356+isezen@users.noreply.github.com>
2022-12-09 08:26:28 +0300

    restructure file names and directories

--- ziesha-installer.sh
+++ ziesha-installer.sh
@@ -31,8 +31,8 @@ clear
 txt=$(red "$header\n" | sed 's/^/                             /')
 echo -e "$txt"
 grn "⬣ Web"; echo -n "      : "; blu "https://ziesha.network\n"
-grn "⬣ Discord"; echo -n "  : "; blu "https://discord.gg/zieshanetwork\n"
 grn "⬣ Github"; echo -n "   : "; blu "https://twitter.com/ZieshaNetwork\n"
+grn "⬣ Discord"; echo -n "  : "; blu "https://discord.gg/zieshanetwork\n"
 grn "⬣ Telegram"; echo -n " : "; blu "https://t.me/ZieshaNetworkTurkish\n"
 line2; echo -e ""
 msg_note "$(ylw "NOTES: ")"; echo -e ""
@@ -41,13 +41,16 @@ cyn " - You can install Uzi-miner as much as computers you want.\n"
 cyn " - You can install Bazuka and Uzi-miner to different computers.\n"
 cyn " - Solo miners, select (3).\n"
 cyn " - Pool operators, select (4).\n"
+line2; echo ""
+msg_info "Ziesha Helper is installed on your system.\n"
+echo -e "  Run ${B}'. ~/.profile'${NONE} or ${C}log out & in${NONE} to be able to run ${R}'ziesha'${NONE} helper."
 line2; echo -e "\n"
+
 
 opts=("Install Bazuka Node"
       "Install Uzi-miner"
       "Solo-miner (Bazuka + Zoro)"
       "Pool operator (Bazuka + Zoro + Uzi-pool)"
-      "Just install Ziesha Helper"
       "Exit")
 PS3=$'\n'$'\033[0;33m'"⬣ What would you like to do?: "
 COLUMNS=1
@@ -66,15 +69,12 @@ do
     "Pool operator (Bazuka + Zoro + Uzi-pool)")
       break
       ;;
-    "Just install Ziesha Helper")
-      break
-      ;;
     "Exit")
         red "-ByE\U02763\U1F60B\n"
       break
       ;;
     *)
-        echo "Invalid $REPLY"
+        echo "Invalid choice $REPLY"
       ;;
   esac
 done
