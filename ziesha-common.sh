d7963b590da737889bdb7db616c8094873233a0b
Ismail SEZEN <613356+isezen@users.noreply.github.com>
2022-12-24 14:09:06 +0300

    Update ziesha-common.sh

--- ziesha-common.sh
+++ ziesha-common.sh
@@ -246,7 +246,7 @@ install () {
     if test -n "$to_install"; then
         if ! is_sudo; then
             msg_warn "You don't have sudo priviliges."; echo
-            ylw "    Install required packages manually:"; echo
+            ylw "   Install required packages manually:"; echo
             col "$EW" "   sudo apt install$to_install"; echo
             return;
         fi
